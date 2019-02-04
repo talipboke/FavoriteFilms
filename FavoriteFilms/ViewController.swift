6a7b39d0853e7b71cb89359bcd79e3cb922a4861
talipboke <talipboke13@gmail.com>
2019-02-04 20:55:22 +0300

    Initial Commit

---
+++ FavoriteFilms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoriteFilms
+//
+//  Created by Talip on 4.02.2019.
+//  Copyright © 2019 Talip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
